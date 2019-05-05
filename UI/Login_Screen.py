71e39fa6fd4d8c5e328917160ac22d83d8c1f634
Clarson1229 <27305718+clarson1229@users.noreply.github.com>
2019-05-04 19:47:57 -0500

    Added Hollow UI to User Screen

--- UI/Login_Screen.py
+++ UI/Login_Screen.py
@@ -14,6 +14,7 @@ def check_secure_val(salt, password, hashval): #Requires that the 'h' is in the 
     if rehashval == hashval:
         return True
     return False
+
 def hash(salt, password):
     #password_salt = uuid.uuid4().hex
      #password_salt = ""
@@ -22,10 +23,11 @@ def hash(salt, password):
     hash.update(('%s%s' % (salt, password)).encode('utf-8'))
     passhash = hash.hexdigest()
     return passhash
-#def generateSalt():
-#   return bcrypt.gensalt()
 
- #passw = '12345'
+# def generateSalt():
+#   return bcrypt.gensalt()
+#
+# passw = '12345'
 # password_salt = generateSalt()
 # password_hash = hash(password_salt, passw)
 #
@@ -43,9 +45,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
  host="localhost",
  user="root",
- password="password", # this will be different depening on your database
+ password="root", # this will be different depening on your database
  database="Project2",
- port = 3306#8889,
+ port = 8889 #3306 ,
  #auth_plugin='mysql_native_password'
 )
 
@@ -138,23 +140,132 @@ def delete_user_not_found_screen():
 def delete_moderator_not_found_screen():
     moderator_not_found_screen.destroy()
 
-
+def add_advertisement():
+    # todo add functionality
+    print('add advertisement')
 
 def user_screen():
     # todo add stuff
+
+    def search_button_click():
+        # todo add functionality
+        print("Search Button Clicked")
+
+    def edit_button_click():
+        # todo add functionality
+        print('Edit button clicked')
+    def delete_button_click():
+        # todo add functionality
+        print('delete button clicked')
+
     main_screen.destroy()
     global user_screen
     user_screen = Tk()  # Create instance
-    user_screen.geometry("700x500")
+    user_screen.geometry("850x600")
     user_screen.title("User Tab")  # Add a title
+
+    global search_verify1
+    search_verify1 = StringVar(user_screen)
+
     Label(text="User Tab", bg="green", width="300", height="2", font=("Calibri", 22)).pack()
-    Label(text="")
+    Label(text="").pack()
+    Button(user_screen, text="Add Advertisements", command=add_advertisement, width=40, height=4).pack()
+
     tab_control = ttk.Notebook(user_screen)  # Create Tab Control
     tab1 = ttk.Frame(tab_control)  # Create a tab
     tab2 = ttk.Frame(tab_control)  # Create a tab
-    tab_control.add(tab1, text='Tab 1')  # Add the tab
-    tab_control.add(tab2, text='Tab 2')  # Add the tab
-    tab_control.pack(expand=1, fill="both")  # Pack to make visible
+
+    tab_control.add(tab1, text='Advertisements ')  # Add the tab
+    tab_control.add(tab2, text='My Advertisements')  # Add the tab
+    ## Advertisements tab
+
+    optionsframe = ttk.Frame(tab1)
+    optionsframe.pack(side="top", fill="x")  # Split tab into two frames top and bottom
+    tableframe = ttk.Frame(tab1)  # Split tab into two frames top and bottom
+    tableframe.pack(side="bottom", fill="x")
+
+    optionsframe.columnconfigure(0, weight=1)
+    optionsframe.columnconfigure(1, weight=1)
+    optionsframe.columnconfigure(2, weight=1)
+    optionsframe.columnconfigure(3, weight=1)
+
+    category_options = [  # Category options menu
+        "All",
+        "Cars and Trucks",
+        "Housing",
+        "Electronics",
+        "Child Care"
+    ]
+    category_verify = StringVar(optionsframe)
+    category_verify.set(category_options[0])  # default value
+    category_entry = OptionMenu(optionsframe, category_verify, *category_options)
+    Label(optionsframe, text="Category").grid(row=0, column=0, padx=20)
+    category_entry.grid(row=1, column=0)
+    Label(text="").pack()
+
+    period_options = [  # Period options menu
+        "Forever",
+        "Past Day",
+        "Past Week",
+        "Past Month",
+        "Past Year"
+    ]
+    period_verify = StringVar(optionsframe)
+    period_verify.set(period_options[0])  # default value
+    period_entry = OptionMenu(optionsframe, category_verify, *period_options)
+    Label(optionsframe, text="Period").grid(row=0, column=1, padx=20)
+    period_entry.grid(row=1, column=1)
+    Label(text="").pack()
+
+
+    Label(optionsframe, text="Title, Description:").grid(row=0, column=2, padx=40)  # Search bar in tab 1
+    search_entry = Entry(optionsframe, textvariable=search_verify1).grid(row=1, column=2, padx=40)
+
+    Button(optionsframe, text="GO", command=search_button_click).grid(row=1, column=3, sticky="W")  # Search button
+
+    advertisements = ttk.Treeview(tableframe)  # advertisements Ads Table
+    advertisements['columns'] = ('title', 'description', 'price', 'date')
+    advertisements['show'] = 'headings'
+    advertisements.heading('title', text='Title')
+    advertisements.column('title', width=125)
+    advertisements.heading('description', text='Description')
+    advertisements.column('description', width=125)
+    advertisements.heading('price', text='Price')
+    advertisements.column('price', width=125)
+    advertisements.heading('date', text='Date')
+    advertisements.column('date', width=125)
+    advertisements.pack()
+
+    ## My advertisements Tab
+    optionsframe2 = ttk.Frame(tab2)
+    optionsframe2.pack(side="top", fill="x")  # Split tab into two frames top and bottom
+
+    tableframe2 = ttk.Frame(tab2)  # Split tab into two frames top and bottom
+    tableframe2.pack(side="bottom", fill="x")
+    optionsframe2.columnconfigure(0, weight=1)
+    optionsframe2.columnconfigure(1, weight=1)
+    Button(optionsframe2, text="EDIT", command=edit_button_click, width= 20, height=4).grid(row=0, column= 0, padx=20 , pady=5)  # Edit  button
+    Button(optionsframe2, text="DELETE", command=delete_button_click, width = 20,  height=4).grid(row=0, column=1, padx=20, pady=5 )  # Delete Search button
+
+    my_advertisements = ttk.Treeview(tableframe2)  # Unclaimed Ads Table
+    my_advertisements['columns'] = ('id','title', 'description','status', 'price', 'date')
+    my_advertisements['show'] = 'headings'
+    my_advertisements.heading('id', text='ID')
+    my_advertisements.column('id', width=125)
+    my_advertisements.heading('title', text='Title')
+    my_advertisements.column('title', width=125)
+    my_advertisements.heading('description', text='Description')
+    my_advertisements.column('description', width=125)
+    my_advertisements.heading('status', text='Status')
+    my_advertisements.column('status', width=125)
+    my_advertisements.heading('price', text='Price')
+    my_advertisements.column('price', width=125)
+    my_advertisements.heading('date', text='Date')
+    my_advertisements.column('date', width=125)
+    my_advertisements.pack()
+
+
+    tab_control.pack(expand=1, fill="both")
     user_screen.mainloop()
 
 
